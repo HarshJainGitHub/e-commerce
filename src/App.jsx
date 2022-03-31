848beb9ed787e1b3c9becf5ced53a299257b8ba4
Harsh Jain <hvj.techture@gmail.com>
2022-03-31 07:56:02 +0530

    routing enabled

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import 'popper.js/dist/umd/popper';
 import 'bootstrap/dist/js/bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from "./NavBar";
-// import CustomersList from './CustomersList';
-// import ShoppingCart from './ShoppingCart';
+import CustomersList from './CustomersList';
+import ShoppingCart from './ShoppingCart';
 import Login from './Login';
-
+import Dashboard from './Dashboard';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import NoMatchPage from './NoMatchPage';
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <BrowserRouter>
         <NavBar />
-        {/* <CustomersList /> */}
-        {/* <ShoppingCart /> */}
-        <Login />
-      </div>
+        <Routes>
+          <Route path="/" exact element={ <Login /> } />
+          <Route path="/cart" exact element={ <ShoppingCart /> } />
+          <Route path="/customers" exact element={ <CustomersList /> } />
+          <Route path="/dashboard" exact element={ <Dashboard /> } />
+          <Route path="*" element={ <NoMatchPage /> } />
+        </Routes>
+      </BrowserRouter>
     )
   }
 }
---
+++ src/NoMatchPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NoMatchPage extends Component {
+  render() {
+    return (
+      <div>
+        <h2> NoMatchPage </h2>
+      </div>
+    )
+  }
+}
